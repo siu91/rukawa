b8d9c5dc90e39f72753b19fc19ef3b29a50aca45
siu <gongshiwen@gmail.com>
2020-03-28 20:31:30 +0800

    feat:初始化p6spy默认配置

--- src/main/java/org/siu/rukawa/datasource/autoconfigure/P6spyAutoConfiguration.java
+++ src/main/java/org/siu/rukawa/datasource/autoconfigure/P6spyAutoConfiguration.java
@@ -4,13 +4,11 @@ import com.p6spy.engine.spy.P6ModuleManager;
 import com.p6spy.engine.spy.P6SpyDriver;
 import com.p6spy.engine.spy.P6SpyOptions;
 import com.p6spy.engine.spy.option.SystemProperties;
+import org.siu.rukawa.datasource.support.P6SpyMessageFormat;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 import java.lang.reflect.Field;
 import java.util.Map;
@@ -37,7 +35,7 @@ public class P6spyAutoConfiguration implements ApplicationRunner {
     public static void p6spyReload() {
         Map<String, String> defaults = P6SpyOptions.getActiveInstance().getDefaults();
         defaults.put(SystemProperties.P6SPY_PREFIX.concat("appender"), "com.p6spy.engine.spy.appender.Slf4JLogger");
-        defaults.put(SystemProperties.P6SPY_PREFIX.concat("logMessageFormat"), "org.siu.rukawa.datasource.P6SpyMessageFormat");
+        defaults.put(SystemProperties.P6SPY_PREFIX.concat("logMessageFormat"), P6SpyMessageFormat.class.getName());
         defaults.put(SystemProperties.P6SPY_PREFIX.concat("customLogMessageFormat"), "executionTime:%(executionTime)| 执行sql:%(sqlSingleLine)");
         P6SpyOptions.getActiveInstance().load(defaults);
         P6ModuleManager.getInstance().reload();
