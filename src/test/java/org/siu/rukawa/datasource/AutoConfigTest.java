936ecdee3807483cade88246c2aa49da3ed63d36
siu <gongshiwen@gmail.com>
2020-03-29 13:20:56 +0800

    移除测试代码到test

--- src/test/java/org/siu/rukawa/datasource/AutoConfigTest.java
+++ src/test/java/org/siu/rukawa/datasource/AutoConfigTest.java
@@ -1,33 +1,20 @@
 package org.siu.rukawa.datasource;
 
 import javax.annotation.Resource;
-import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.siu.rukawa.datasource.autoconfigure.properties.DataSourceProperty;
-import org.siu.rukawa.datasource.autoconfigure.properties.DynamicDataSourceProperties;
-import org.siu.rukawa.datasource.core.DynamicRoutingDataSource;
 import org.siu.rukawa.datasource.core.event.AddDataSourceEvent;
 import org.siu.rukawa.datasource.core.event.EventPublisher;
-import org.siu.rukawa.datasource.core.provider.DataSourceProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("rukawa-test-hfashjfsdkljfkl")
 @SpringBootTest(classes = Application.class,webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class ApplicationTest {
-
-    @Autowired
-    private WebApplicationContext wac;
-
+public class AutoConfigTest {
 
     @Resource
     EventPublisher eventPublisher;
